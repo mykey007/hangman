a9c7101728b2bcbf046e48e3a887a247642311da
Michael Mallowe <mmallowe@gmail.com>
2020-04-22 16:58:04 -0700

    add gameover logic show/hide word & btns

--- src/Hangman.js
+++ src/Hangman.js
@@ -58,16 +58,19 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    const gameOver = this.state.nWrong >= this.props.maxWrong;
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
         <p>Guessed Wrong: {this.state.nWrong}</p>
-        <p className='Hangman-word'>{this.guessedWord()}</p>
+        <p className='Hangman-word'>
+          {!gameOver ? this.guessedWord() : this.state.answer}
+        </p>
         <p className='Hangman-btns'>
-          {this.state.nWrong < this.props.maxWrong 
+          {!gameOver 
             ? this.generateButtons() 
-            : `You lose: ${this.state.answer}`}  
+            : `Try Again!`}  
           {/* {this.generateButtons()} */}
         </p>
       </div>
